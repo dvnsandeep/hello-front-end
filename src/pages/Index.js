559758c0c1b8b92f862aa71e0e2401a3067fe211
gopavasanth <gopavasanth1999@gmail.com>
2019-06-20 17:58:37 +0530

    First Commit hello-front-end

---
+++ src/pages/Index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../pages/Home';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Home/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Index;

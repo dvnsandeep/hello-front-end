036140a46eeba05a74eeb571f890f0379ceb1bdc
abhinavdv <dvabhinav31@gmail.com>
2019-07-31 01:36:29 +0530

    A distance function in map.js to calculate distance given latitude and longitude

--- src/components/dashboard/Map.js
+++ src/components/dashboard/Map.js
@@ -7,6 +7,38 @@ class Map extends Component {
         directions: null
     };
 
+    
+    distance(lat1, lon1, lat2, lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            var radlat1 = Math.PI * lat1 / 180;
+            var radlat2 = Math.PI * lat2 / 180;
+            var theta = lon1 - lon2;
+            var radtheta = Math.PI * theta / 180;
+            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+            if (dist > 1) {
+                dist = 1;
+            }
+            dist = Math.acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.609344;
+            return dist;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     componentDidMount() {
         const directionsService = new google.maps.DirectionsService();
 
